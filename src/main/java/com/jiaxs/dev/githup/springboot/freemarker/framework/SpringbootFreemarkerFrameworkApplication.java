67744ba4c3775a9c41a1d7840370f73aad6967c8
jiaxun19920218 <2572104454@qq.com>
2021-07-26 14:25:25 +0800

    project init

---
+++ src/main/java/com/jiaxs/dev/githup/springboot/freemarker/framework/SpringbootFreemarkerFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.jiaxs.dev.githup.springboot.freemarker.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFreemarkerFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFreemarkerFrameworkApplication.class, args);
+	}
+
+}
